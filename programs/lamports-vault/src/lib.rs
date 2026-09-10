9f80a070f3599a759a0f3e85665a00203854fd4f
bjavier-tester <bjavier@umich.edu>
2026-09-10 16:37:31 -0400

    Update lib.rs

--- programs/lamports-vault/src/lib.rs
+++ programs/lamports-vault/src/lib.rs
@@ -15,16 +15,16 @@ declare_id!("9AvGuh5C8cYcYU7RwwWQU9iDFqWpjQ9MnGZ8cfbkJPLc");
 pub mod lamports_vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::initialize_vault(ctx)
+    pub fn initialize(ctx: Context<Initialize>, max_withdraw: u64) -> Result<()> {
+        initialize::initialize_vault(ctx, max_withdraw)
     }
 
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
         deposit::deposit_lamports(ctx, amount)
     }
 
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_withdraw: u64) -> Result<()> {
-        withdraw::withdraw_lamports(ctx, amount, max_withdraw)
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        withdraw::withdraw_lamports(ctx, amount)
     }
 
     pub fn close(ctx: Context<Close>) -> Result<()> {
